e32a0b9a8700b733cdd3badc000a1817a05dd54e
sulagnas <sulagnadas1996@gmail.com>
2018-01-12 13:45:57 +0530

    user have to login first to view their todo list

--- server.js
+++ server.js
@@ -2,8 +2,35 @@ const http = require('http');
 const WebApp = require('./webapp');
 
 const serveFile=require('./serverLib/staticFileHandler');
+
+let toS = o=>JSON.stringify(o,null,2);
+
+let logRequest = (req,res)=>{
+  let text = ['------------------------------',
+    `${timeStamp()}`,
+    `${req.method} ${req.url}`,
+    `HEADERS=> ${toS(req.headers)}`,
+    `COOKIES=> ${toS(req.cookies)}`,
+    `BODY=> ${toS(req.body)}`,''].join('\n');
+  fs.appendFile('request.log',text,()=>{});
+  console.log(`${req.method} ${req.url}`);
+};
+
+let loadUser = (req,res)=>{
+  let sessionid = req.cookies.sessionid;
+  let user = registered_users.find(u=>u.sessionid==sessionid);
+  if(sessionid && user){
+    req.user = user;
+  }
+};
+
+const redirectToLoginPage=function (req,res) {
+  res.redirect('./loginPage.html');
+}
+
 let app = WebApp.create();
 
+app.get('/yourTodo.html',redirectToLoginPage);
 app.useAsPostProcessor(serveFile);
 
 const PORT = 4000;
